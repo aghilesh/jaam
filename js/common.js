a81fdbd0b5a13400f35937f9d3fbe41978ed1920
Manaf P M <manaftvm@gmail.com>
2014-03-14 00:33:36 +0530

    university pages

---
+++ js/common.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+jQuery( document ).ready(function() {
+    jQuery('.common-add-btn-click').on('click',function(){
+        jQuery('.generic-add-form-slider').slideDown('slow');
+    })
+});
