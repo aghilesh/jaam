91bc0b758e74443f839faaef0361473098dc9965
Abhilash V <blueeyes4ya@gmail.com>
2014-04-13 21:31:41 +0530

    university courses

---
+++ application/views/pages/university_courses/edit.php
@@ -0,0 +1,54 @@
+<?php
+$post = $this->input->post();
+?>
+<div class="fl full-width">
+    <div class="form-inner">
+        <?php
+        $attributes = array('class' => 'validate-form', 'id' => 'frmUniversityAdd','name'=>'frmUniversityAdd');
+        echo form_open($paths['add'], $attributes);
+        ?>
+        <ul class="fl form-fields-ul-slider">
+            <li class="fl form-label">Code<sup class="mandatory">*</sup></li>
+            <li class="fl form-field"><input class="generic-input validate validate-mandatory" type="text" name="code" id="code" placeholder="Code" value="<?php echo $university->code?>"/></li>
+            <li class="clear newline"/>
+
+            <li class="fl form-label">University<sup class="mandatory">*</sup></li>
+            <li class="fl form-field"><?php echo form_dropdown('university_id', $universities, $university->university_id,'id="university_id" class="generic-input"');?></li>
+            <li class="clear newline"/>
+
+            <li class="fl form-label">Country</li>
+            <li class="fl form-field"><?php echo form_dropdown('country_id', $countries, $university->country_id,'id="country_id" class="generic-input"');?></li>
+            <li class="clear newline"/>
+
+            <li class="fl form-label">Course title<sup class="mandatory">*</sup></li>
+            <li class="fl form-field"><input class="generic-input validate validate-mandatory" type="text" name="course_title" id="course_title" placeholder="Course title" value="<?php echo $university->course_title?>"/></li>
+            <li class="clear newline"/>
+
+            <li class="fl form-label">Course description</li>
+            <li class="fl form-field"><input class="generic-input" type="text" name="course_description" id="course_description" placeholder="Course description"  value="<?php echo $university->course_descripition?>"/></li>
+            <li class="clear newline"/>
+        </ul>
+        <ul class="fl form-fields-ul-slider">
+            
+            <li class="fl form-label">Course level</li>
+            <li class="fl form-field"><input class="generic-input" type="text" name="course_level" id="email_id" placeholder="Course level" value="<?php echo $university->course_level?>"/></li>
+            <li class="clear newline"/>
+            
+            <li class="fl form-label">Duration</li>
+            <li class="fl form-field"><input class="generic-input" type="text" name="duration" id="duration" placeholder="Duration" value="<?php echo $university->duration?>"/></li>
+            <li class="clear newline"/>
+
+            <li class="fl form-label">Course fee</li>
+            <li class="fl form-field"><input class="generic-input" type="text" name="course_fee" id="course_fee" placeholder="Course fee" value="<?php echo $university->course_fee?>"/></li>
+            <li class="clear newline"/>
+
+            <li class="fl form-label">&nbsp;</li>
+            <li class="fl form-field" sty>
+                <a href="<?php echo $this->config->item('base_url').$paths['list'] ?>" class="fl marginleft10 button cancel common-cancel-btn-click">Cancel</a>
+                <a href="javascript:void(0)" class="fl button save common-save-btn-click">Save</a>
+            </li>
+            <li class="clear newline"/>
+        </ul>
+        <?php echo form_close(); ?>
+    </div>
+</div>
---
+++ application/views/pages/university_courses/list.php
@@ -0,0 +1,32 @@
+<table class="fl table generic-listing">
+    <thead>
+        <tr>
+            <th>Sl.No.</th>
+            <th>Course Code</th>
+            <th>Course Title</th>
+            <th><a href="<?php echo base_url() . $paths['add'] ?>" class="fr button common-add-btn-click">ADD</a></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        if (isset($departments) && !empty($departments)) {
+            $sl = 1;
+            foreach ($departments as $list) {
+                ?>
+                <tr>
+                    <td><?php echo $sl; ?></td>
+                    <td><?php echo $list->code; ?></td>
+                    <td><?php echo $list->course_title; ?></td>
+                    <td>
+                        <a href="javascript:void(0)" class="button remove delete" data-link="<?php echo base_url() . $paths['delete'] . '/' . $list->id; ?>">Remove</a>
+                        <a href="<?php echo base_url() . $paths['edit'] . '/' . $list->id; ?>" class="button edit">Edit</a>
+                    </td>
+                </tr>
+                <?php
+                $sl++;
+            }
+        } else {
+            ?>
+            <tr><td colspan="5">No Departments found.</td></tr>
+        <?php } ?>
+</table>
